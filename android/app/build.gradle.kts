a90c202134e41b35167b1fe39fbc90fc5a9aa755
Sakshyam11111 <sakshyamshrestha111@gmail.com>
2025-09-12 14:48:55 +0545

    build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id 'com.google.gms.google-services'
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,7 +44,7 @@ flutter {
     source = "../.."
 }
 
-dependencies{
-    implementation platform('com.google.firebase:firebase-bom:34.2.0')
-    implementation 'com.google.firebase:firebase-analytics'
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
-dependencies {
-    classpath 'com.google.gms.google-services:4.4.3'
+plugins {
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 allprojects {
@@ -10,12 +10,13 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
